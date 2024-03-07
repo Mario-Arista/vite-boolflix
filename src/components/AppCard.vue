303f517a71b5c1b86ebfc0ba07d5cfc1f8051672
mario.arista <mario.arista.dev@gmail.com>
2024-03-07 19:54:23 +0100

    images inserted and hoover effect

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -63,26 +63,34 @@ export default {
         class="card"
         v-if="posterMovie"
     >
-        <div class="original-title"><strong>Titolo originale:</strong> <span>{{ posterMovie.original_title }}</span></div>
-        <div class="non-original-title"><strong>Titolo:</strong> <span>{{ posterMovie.title }}</span></div>
-        <div class="original-language">
-            <img :src="changeFlagUrlMovie(posterMovie)" :alt="posterMovie.original_language">
+        <div class="poster-container">
+            <img :src="'https://image.tmdb.org/t/p/w342/' + posterMovie.poster_path" :alt="posterMovie.original_title">
         </div>
-        <div class="vote-avarage"><strong>Rating:</strong> <span>{{ posterMovie.vote_average.toFixed(2) }}</span></div>
-
+        <div class="hidden-info">
+            <div class="original-title"><strong>Titolo originale:</strong> <span>{{ posterMovie.original_title }}</span></div>
+            <div class="non-original-title"><strong>Titolo:</strong> <span>{{ posterMovie.title }}</span></div>
+            <div class="original-language">
+                <img :src="changeFlagUrlMovie(posterMovie)" :alt="posterMovie.original_language">
+            </div>
+            <div class="vote-avarage"><strong>Rating:</strong> <span>{{ posterMovie.vote_average.toFixed(2) }}</span></div>
+        </div>
     </div>
 
     <div 
         class="card"
         v-if="posterSerieTv"
     >
-        <div class="original-title"><strong>Titolo originale:</strong> <span>{{ posterSerieTv.original_name }}</span></div>
-        <div class="name-serie-tv"><strong>Titolo:</strong> <span>{{ posterSerieTv.name }}</span></div>
-        <div class="original-language">
-            <img :src="changeFlagUrlSerieTv(posterSerieTv)" :alt="posterSerieTv.original_language">
+        <div class="poster-container">
+            <img :src="'https://image.tmdb.org/t/p/w342/' + posterSerieTv.backdrop_path" :alt="posterSerieTv.original_name">
         </div>
-        <div class="vote-avarage"><strong>Rating:</strong> <span>{{ posterSerieTv.vote_average.toFixed(2) }}</span></div>
-
+        <div class="hidden-info">
+            <div class="original-title"><strong>Titolo originale:</strong> <span>{{ posterSerieTv.original_name }}</span></div>
+            <div class="name-serie-tv"><strong>Titolo:</strong> <span>{{ posterSerieTv.name }}</span></div>
+            <div class="original-language">
+                <img :src="changeFlagUrlSerieTv(posterSerieTv)" :alt="posterSerieTv.original_language">
+            </div>
+            <div class="vote-avarage"><strong>Rating:</strong> <span>{{ posterSerieTv.vote_average.toFixed(2) }}</span></div>
+        </div>
     </div>
 
 
@@ -96,23 +104,51 @@ export default {
 
 
 .card {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    gap: 10px;
-    padding-left: 5px;
+
+    width: calc(100% / 5 - $cardOrizontalGap / 5 * 4);
+
+    overflow-y: auto;
 
     background-color: transparent;
-    width: calc(100% / 5 - $cardOrizontalGap / 5 * 4);
 
     // Test
     border: 1px dashed black;
+
+    &:hover {
+
+        .poster-container {
+            display: none;
+        }
+
+        .hidden-info {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            gap: 10px;
+            padding-left: 5px;
+            padding-top: 5px;
+
+        }
+
+    }
+
+    .poster-container {
+
+        img {
+            display: block;
+            width: 100%;
+        }
+    }
+
+    .hidden-info {
+
+        display: none;
+    }
 
     strong {
         color: $primary_color;
     }
 }
-
 
 
 </style>
